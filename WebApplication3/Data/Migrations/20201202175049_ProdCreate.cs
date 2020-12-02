9125318f1042a000c213c1d49dba825d79a65986
rachwongrw <rwrachelwong@gmail.com>
2020-12-02 10:18:46 -0800

    add sql server

---
+++ WebApplication3/Data/Migrations/20201202175049_ProdCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Data.Migrations
+{
+    public partial class ProdCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "CreatedOn", "Description", "IsComplete", "Priority" },
+                values: new object[] { 1, new DateTime(2020, 12, 2, 9, 50, 49, 367, DateTimeKind.Local).AddTicks(4971), "Bake Cookies", false, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "CreatedOn", "Description", "IsComplete", "Priority" },
+                values: new object[] { 2, new DateTime(2020, 12, 2, 9, 50, 49, 376, DateTimeKind.Local).AddTicks(2730), "Eat Cookies", false, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "CreatedOn", "Description", "IsComplete", "Priority" },
+                values: new object[] { 3, new DateTime(2020, 12, 2, 9, 50, 49, 376, DateTimeKind.Local).AddTicks(3162), "Run around the block", false, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
